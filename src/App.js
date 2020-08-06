3710d902a46c345114d1d049324e990d08846883
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-08-06 10:09:52 +0200

    minor fixes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { Canvas } from "react-three-fiber";
 import { Html, Stats } from "drei";
 import Effects from "./Effects";
 import Scene from "./Scene";
-import { Controls } from 'react-three-gui';
+import { Controls } from "react-three-gui";
 
 function App() {
-  const showControls = window.location.search.includes("ctrl")
+  const showControls = window.location.search.includes("ctrl");
 
   return (
     <>
@@ -17,24 +17,37 @@ function App() {
         style={{
           background: "#121212",
         }}
-        gl={{ powerPreference: 'high-performance', alpha: false, antialias: false, stencil: false, depth: false }}
+        gl={{
+          powerPreference: "high-performance",
+          alpha: false,
+          antialias: false,
+          stencil: false,
+          depth: false,
+        }}
       >
-
         {showControls && <Stats />}
-        
+
         <Effects />
-        <Suspense fallback={<Html center><span className="loading">Loading.</span></Html>}>
+        <Suspense
+          fallback={
+            <Html center>
+              <span className="loading">Loading.</span>
+            </Html>
+          }
+        >
           <Scene />
           <Html center>
-            <div className="title">
-              TAKE CONTROL
-            </div>
+            <div className="title">TAKE CONTROL</div>
           </Html>
         </Suspense>
       </Canvas>
 
-      {showControls && <div className="controls"> <Controls /> </div>}
-      
+      {showControls && (
+        <div className="controls">
+          {" "}
+          <Controls />{" "}
+        </div>
+      )}
     </>
   );
 }
